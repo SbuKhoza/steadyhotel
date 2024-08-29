37e33aebfe584bebb1a58c18c03a57d26c57eabf
sibusiso <sibusisok59@gmail.com>
2024-08-29 13:52:17 +0200

    sign up functionality updated

---
+++ .history/src/components/LoginSignup/LoginSignup_20240829131556.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './LoginSignup.css';
+
+ function LoginSignup() {
+  return (
+    <div className='containersign'>
+    <div className='logsigncont'>
+
+    <div className='logimg'>
+        <img src='login.jpg' alt='login'></img>
+    </div>
+
+    <div className='logsign'>
+        <div className='signhearder'>
+            <div className='headtext'>
+                <h2>Sign Up </h2>
+            </div>
+            <div className='underline'></div>
+        </div>
+
+        <div className='inputs'>
+            <div className='fname'> 
+                <label htmlFor='name'>Name: </label><br/>
+                <input type='text' placeholder='Sbuda Malloya'></input>
+            </div>
+        </div>
+
+        <div className='inputs'>
+            <div className='uname'> 
+                <label htmlFor='username'>User Name: </label><br/>
+                <input type='text' placeholder='Malloya'></input>
+            </div>
+        </div>
+
+        <div className='inputs'>
+            <div className='mail'> 
+                <label htmlFor='name'>Email: </label><br/>
+                <input type='text' placeholder='sbuda@mail.com'></input>
+            </div>
+        </div>
+
+        <div className='inputs'>
+            <div className='pass'> 
+                <label htmlFor='password'>Name: </label><br/>
+                <input type='password' placeholder='p@**s***5'></input>
+            </div>
+        </div>
+
+        <div className='inputs'>
+            <div className='confpass'> 
+                <label htmlFor='password'>Name: </label><br/>
+                <input type='password' placeholder='p@**s***5'></input>
+            </div>
+        </div>
+
+        <div className='submitcont'>
+            
+            <div className='submit'>Sign up</div>
+            <div className='submit'>Login</div>
+        </div>
+    
+    
+    
+    </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default LoginSignup;
